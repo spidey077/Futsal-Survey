b5dbc9dbb5efee290ab0a24c372042a62ece9ed1
Imdadullah <137488875+spidey077@users.noreply.github.com>
2025-03-08 12:43:06 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("survey-form");
-
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        location.reload();
-    });
-});
